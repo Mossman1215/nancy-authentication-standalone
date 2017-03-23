c59c8629d5ac80a2184e6b4ebf96b8db8da07957
Mossman1215 <mossman2142@gmail.com>
2017-03-23 21:10:17 +1300

    initial commit for upload

---
+++ UserValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace Nancy.Demo.Authentication.Basic
+{
+    using System.Collections.Generic;
+    using Nancy.Authentication.Basic;
+    using Nancy.Security;
+
+    public class UserValidator : IUserValidator
+    {
+        public IUserIdentity Validate(string username, string password)
+        {
+            if (username == "demo" && password == "demo")
+            {
+                return new user { UserName = username, Claims = new List<string> {"admin" } };
+            }
+
+            // Not recognised => anonymous.
+            return null;
+        }
+
+        
+    }
+}
---
+++ user.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy.Security;
+
+namespace Nancy.Demo.Authentication.Basic
+{
+    public class user : IUserIdentity
+    {
+        public string UserName { get; set; }
+        public IEnumerable<string> Claims { get; set; }
+    }
+}
